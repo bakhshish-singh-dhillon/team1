95aacca30c8ffb69dc144bd406c60be62516b658
Bakhshish Singh Dhillon <bakhshish.dev@gmail.com>
2022-07-16 18:02:29 -0500

    Transaction Table Completed

---
+++ farm-fresh/database/migrations/2022_07_16_225445_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('order_id');
+            $table->string('cc_num');
+            $table->string('payment_transaction_id');
+            $table->string('status');
+            $table->string('response');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
