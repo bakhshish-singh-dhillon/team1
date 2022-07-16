090a52cf6dabd2cc3f37ecbeeb9b95f373c96e6c
Bakhshish Singh Dhillon <bakhshish.dev@gmail.com>
2022-07-16 15:55:17 -0500

    Product Table Migration Completed

--- farm-fresh/database/migrations/2022_07_16_193936_create_products_table.php
+++ farm-fresh/database/migrations/2022_07_16_193936_create_products_table.php
@@ -15,6 +15,12 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('sku');
+            $table->string('name');
+            $table->decimal('price');
+            $table->string('description');
+            $table->string('measure_unit');
+            $table->string('quantity');
             $table->softDeletes();
             $table->timestamps();
         });
