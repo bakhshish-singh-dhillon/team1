28f2ba969993f77e2e90d41bd7f6691fc30e24dd
Pulkit Bhardwaj <pulkitbhardwaj3380@gmail.com>
2022-07-18 14:16:44 -0500

    Last Paragraph Complete

--- farm-fresh/resources/views/about.blade.php
+++ farm-fresh/resources/views/about.blade.php
@@ -10,6 +10,7 @@
                     <h1>About Us</h1>
                     <p>Farm Fresh began from a personal need and has gone on to explore the subtleties in the business of good food. Over the years, we studied canadian consumers behavior and worked on a strategy that has been a good one so far. Unless the consumers are connected to ground reality, he/she is not going to consume good food products.</p>
                     <p>This was our goal when we started to explore the Good Food markets in the United States of America and came back with a determination to connect small & marginal farmers across Canada with consumers.</p>
+                    <p>Over the past few years, Farm Fresh has developed a network of more than 100,000 small farmers across Canada. We provided these farmers training, mentoring and direct access to the market, thus providing them better value for their produce and improving their bottom line. We operate Organic Food stores as well as distribute food to processed food makers. </p>
                 </div>
         </div>
     </div>
