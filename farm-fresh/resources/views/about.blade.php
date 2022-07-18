8786f8f413077728e5951a740da7e655474f06ce
Pulkit Bhardwaj <pulkitbhardwaj3380@gmail.com>
2022-07-18 14:10:15 -0500

    Paragraph 1 Complete

--- farm-fresh/resources/views/about.blade.php
+++ farm-fresh/resources/views/about.blade.php
@@ -2,15 +2,14 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row justify-content-center text-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
                     <h1>About Us</h1>
+                    <p>Farm Fresh began from a personal need and has gone on to explore the subtleties in the business of good food. Over the years, we studied canadian consumers behavior and worked on a strategy that has been a good one so far. Unless the consumers are connected to ground reality, he/she is not going to consume good food products.</p>
                 </div>
-            </div>
         </div>
     </div>
 </div>
