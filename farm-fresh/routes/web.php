3e9e23cba4c5cf99711feee8241c733ffcc05e9d
Bakhshish Singh Dhillon <bakhshish.dev@gmail.com>
2022-07-17 19:17:05 -0500

    Product routes created

--- farm-fresh/routes/web.php
+++ farm-fresh/routes/web.php
@@ -21,3 +21,12 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/admin', [AdminProductsController::class, 'index'])->name('dashboard');
+Route::get('/admin/products', [AdminProductsController::class, 'index']);
+Route::get('/admin/products/create', [AdminProductsController::class, 'create']);
+Route::post('/admin/products', [AdminProductsController::class, 'store']);
+Route::get('/admin/products/{product}/edit', [AdminProductsController::class, 'edit'])->name('product-edit');
+Route::put('/admin/products/{product}', [AdminProductsController::class, 'update'])->name('product-update');
+Route::delete('/admin/products/{product}', [AdminProductsController::class, 'destroy'])->name('product-delete');
