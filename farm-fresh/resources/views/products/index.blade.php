847def629db4f11b2edc7c694e218c67959e35ba
Sargam <sargam.sanghani@gmail.com>
2022-07-19 14:53:18 -0500

    Details page designing done

--- farm-fresh/resources/views/products/index.blade.php
+++ farm-fresh/resources/views/products/index.blade.php
@@ -27,13 +27,50 @@
             </div>
             <div class="col-md-9">
                 <div class="title">All Products (4)</div>
-                <div class="card col-md-4" style="width: 18rem;">
-                    <img class="card-img-top" src="images/placeholder.png" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title">Apple</h5>
-                        <p class="card-text">$ 5 / lb</p>
-                        <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn">Go somewhere</a>
+                <div class="row">
+                    <div class="col-md-4 my-2">
+                        <div class="card">
+                            <img class="card-img-top" src="images/placeholder.png" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Apple</h5>
+                                <p class="card-text">$ 5 / lb</p>
+                                <p class="ellipsis-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn">View</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 my-2">
+                        <div class="card">
+                            <img class="card-img-top" src="images/placeholder.png" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Apple</h5>
+                                <p class="card-text">$ 5 / lb</p>
+                                <p class="ellipsis-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn">View</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 my-2">
+                        <div class="card">
+                            <img class="card-img-top" src="images/placeholder.png" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Apple</h5>
+                                <p class="card-text">$ 5 / lb</p>
+                                <p class="ellipsis-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn">View</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 my-2">
+                        <div class="card">
+                            <img class="card-img-top" src="images/placeholder.png" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Apple</h5>
+                                <p class="card-text">$ 5 / lb</p>
+                                <p class="ellipsis-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn">View</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
