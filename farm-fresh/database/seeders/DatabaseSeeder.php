080d459e53c3e1626b3729a5e8451efbab0601ab
Dhara Patel <dhara@Dharas-MacBook-Air.local>
2022-07-19 10:42:58 -0500

    database seeder edited and product seeder added

--- farm-fresh/database/seeders/DatabaseSeeder.php
+++ farm-fresh/database/seeders/DatabaseSeeder.php
@@ -16,5 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(UsersTableSeeder::class);
+        $this->call(ProductSeeder::class);
+        $this->call(ImageSeeder::class);
     }
 }
