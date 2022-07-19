ba0d01ca26b1a0a3cba7ab8767c35c41badc0d53
pravindra <singh280597@gmail.com>
2022-07-19 15:15:18 -0500

    Added select field in contact form

--- farm-fresh/resources/views/contact/index.blade.php
+++ farm-fresh/resources/views/contact/index.blade.php
@@ -1,34 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="max-container py-4">
-    <div class="row align-items-stretch no-gutters contact-wrap">
-        <div class="col-md-6 px-4">
+<div class="max-container py-4 my-4">
+    <div class="row align-items-stretch no-gutters contact-wrap normal-shadow">
+        <div class="col-md-7 px-4 py-4">
             <div class="form h-100">
                 <h3>Send us a message</h3>
                 <form class="mb-5" method="post" id="contactForm" name="contactForm">
                     <div class="row">
-                        <div class="col-md-6 form-group mb-5">
+                        <div class="col-md-12 form-group mb-3">
                             <label for="" class="col-form-label">Name *</label>
                             <input type="text" class="form-control" name="name" id="name" placeholder="Your name">
                         </div>
-                        <div class="col-md-6 form-group mb-5">
+                        <div class="col-md-12 form-group mb-3">
                             <label for="" class="col-form-label">Email *</label>
                             <input type="text" class="form-control" name="email" id="email" placeholder="Your email">
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-md-6 form-group mb-5">
+                        <div class="col-md-12 form-group mb-3">
                             <label for="" class="col-form-label">Phone</label>
                             <input type="text" class="form-control" name="phone" id="phone" placeholder="Phone #">
                         </div>
-                        <div class="col-md-6 form-group mb-5">
-                            <label for="" class="col-form-label">Company</label>
-                            <input type="text" class="form-control" name="company" id="company" placeholder="Company  name">
+                        <div class="col-md-12 form-group mb-3">
+                            <label for="" class="col-form-label">How can we help you?</label></br>
+                            <!--<input type="text" class="form-control" name="company" id="company" placeholder="Company  name"> -->
+                            <select class="form-select" id="category" name="category" required="required">
+                                <option value="" selected disabled>Select below *</option>
+                                <option value="refund">Refund</option>
+                                <option value="order_status">Order Status</option>
+                                <option value="feedback">Feedback</option>
+                                <option value="other">Other</option>
+                            </select>
                         </div>
+
+                        </select>
                     </div>
                     <div class="row">
-                        <div class="col-md-12 form-group mb-5">
+                        <div class="col-md-12 form-group mb-3">
                             <label for="message" class="col-form-label">Message *</label>
                             <textarea class="form-control" name="message" id="message" cols="30" rows="4" placeholder="Write your message"></textarea>
                         </div>
@@ -46,8 +55,8 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-6 px-4">
-            <div class="contact-info h-100 px-4">
+        <div class="col-md-5 px-0">
+            <div class="contact-info h-100 px-4 bg-green text-white py-4">
                 <h3>Contact Information</h3>
                 <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, magnam!</p>
                 <ul class="list-unstyled">
