c4b540976bc12ff9a4a83b82c4f6511d23f30660
Pulkit Bhardwaj <pulkitbhardwaj3380@gmail.com>
2022-07-19 09:21:30 -0500

    About page work done

--- farm-fresh/resources/views/about.blade.php
+++ farm-fresh/resources/views/about.blade.php
@@ -7,6 +7,7 @@
             <div class="card">
 
                 <div class="card-body">
+                    {{-- About Page Content Complete  --}}
                     <h1>About Us</h1>
                     <p>Farm Fresh began from a personal need and has gone on to explore the subtleties in the business of good food. Over the years, we studied canadian consumers behavior and worked on a strategy that has been a good one so far. Unless the consumers are connected to ground reality, he/she is not going to consume good food products.</p>
                     <p>This was our goal when we started to explore the Good Food markets in the United States of America and came back with a determination to connect small & marginal farmers across Canada with consumers.</p>
