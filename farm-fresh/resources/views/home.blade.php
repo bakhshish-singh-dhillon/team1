3669a35e19cfa94f5b28acbc2b5ded29b2a5e0f1
Sargam <sargam.sanghani@gmail.com>
2022-07-19 11:04:36 -0500

    Added variable for common theme

--- farm-fresh/resources/views/home.blade.php
+++ farm-fresh/resources/views/home.blade.php
@@ -3,7 +3,11 @@
 @section('content')
 
 <div>
-    <img src="images/main-bg.jpg" alt="Home">
+    <img src="images/main-bg.jpg" alt="Home" />
+
+    <div id="category">
+        <div class="title py-3">Browse by Category</div>
+    </div>
 </div>
 
 @endsection
