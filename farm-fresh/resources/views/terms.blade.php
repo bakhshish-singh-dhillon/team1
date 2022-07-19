be3b87786ac687f25937a964b91ffaafc5d73697
Pulkit Bhardwaj <pulkitbhardwaj3380@gmail.com>
2022-07-19 12:49:04 -0500

    Terms and Condition work complete.

--- farm-fresh/resources/views/terms.blade.php
+++ farm-fresh/resources/views/terms.blade.php
@@ -36,7 +36,15 @@
                             Notice and Procedure for Notifying farmfresh.com of Objectionable Content
                         </li>
                     </ul>
+                    <h3>Conditions Relating to Your Use of farmfresh.com</h3>
+                    <p>
+                        If you use the website, you are responsible for maintaining the confidentiality of your account and password and for restricting access to your computer to prevent unauthorised access to your account. You agree to accept responsibility for all activities that occur under your account or password. You should take all necessary steps to ensure that the password is kept confidential and secure and should inform us immediately if you have any reason to believe that your password has become known to anyone else, or if the password is being, or is likely to be, used in an unauthorised manner. Please ensure that the details you provide us with are correct and complete and inform us immediately of any changes to the information that you provided when registering. You can access and update much of the information you provided us with in the Your Account area of the website. You agree and acknowledge that you will use your account on the website to purchase products only for your personal use and not for business purposes. Should you wish to order products for business purposes, please create a vendor account on farmfresh.com.
+                    </p>
                     </div>
+                    <h4>Privacy</h4>
+                    <p>
+                        Please review our Privacy Notice, which also governs your visit to farmfresh.com, to understand our practices. The personal information / data provided to us by you during the course of usage of farmfresh.com will be treated as strictly confidential and in accordance with the Privacy Notice and applicable laws and regulations. If you object to your information being transferred or used, please do not use the website.
+                    </p>
         </div>
     </div>
 </div>
