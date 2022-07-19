18508eecdb55a2822b192354b40dce5ff9b69f15
Sargam <sargam.sanghani@gmail.com>
2022-07-19 14:44:54 -0500

    Categories listed done

--- farm-fresh/resources/views/products/index.blade.php
+++ farm-fresh/resources/views/products/index.blade.php
@@ -1,24 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Products</div>
 
-                <div class="card-body">
-                    <table id="" class="table table-striped">
-                        <tr>
 
-                            <th>Name</th>
-                            <th>Sku</th>
-                            <th>Price</th>
-                            <th>Quantity</th>
-                            <th>Description</th>
+<div id="products">
+    <div class="max-container py-4">
+        <div class="row">
+            <div class="col-md-3 left-part">
+                <div class="title">Categories</div>
 
-                        </tr>
-                    </table>
+                <div class="category-list">
+                    <ul class="p-0">
+                        <li>
+                            <a href="" class="my-2"><strong>All Products</strong></a>
+                        </li>
+                        <li><a href=""><strong>Fruits</strong></a>
+                            <ul class="">
+                                <li><a href="">Tropical</a></li>
+                                <li><a href="">Berries</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="" class="my-2"><strong>Veggies</strong></a></li>
+                        <li><a href="" class="my-2"><strong>Dairy Products</strong></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-9">
+                <div class="title">All Products (4)</div>
+                <div class="card col-md-4" style="width: 18rem;">
+                    <img class="card-img-top" src="images/placeholder.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Apple</h5>
+                        <p class="card-text">$ 5 / lb</p>
+                        <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn">Go somewhere</a>
+                    </div>
                 </div>
             </div>
         </div>
