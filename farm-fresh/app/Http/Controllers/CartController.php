25daafd2e9ecd81bc2595556ef5d79f4754599f4
Bakhshish Singh Dhillon <bakhshish.dev@gmail.com>
2022-07-21 17:09:01 -0500

    Cart Controller created

---
+++ farm-fresh/app/Http/Controllers/CartController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Product;
+
+class CartController extends Controller
+{
+    public function add(Product $product)
+    {
+        $cart = session()->get('cart', []);
+
+        if (isset($cart[$product->id])) {
+            $cart[$product->id]['quantity']++;
+        } else {
+            $cart[$product->id] = [
+                "quantity" => 1,
+                "name" => $product->name,
+                "price" => $product->price
+            ];
+        }
+
+        session()->put('cart', $cart);
+        return redirect()->withSuccess('success', 'Product added to cart successfully!');
+    }
+
+}
