dcdb81b375603a5f41c38f5497299c9326aa64c7
Dhara Patel <dhara@Dharas-MacBook-Air.local>
2022-07-21 10:40:16 -0500

    image path edited

--- farm-fresh/resources/views/products/show.blade.php
+++ farm-fresh/resources/views/products/show.blade.php
@@ -72,7 +72,7 @@
                 <div class="col-md-3 px-4">
                     <div class="card product-item">
 
-                        <img class="card-img-top" src="/images/products/{{$prod->images()->first()->url}}" alt="{{$prod->images()->first()->url}}">
+                        <img class="card-img-top" src="{{$images_path.$prod->images()->first()->url}}" alt="{{$prod->images()->first()->url}}">
                         <div class="card-body">
                             <h5 class="card-title green-text text-bold">{{$prod->name}}</h5>
                             <p class="card-text">$ {{$prod->price}} / {{$prod->measure_unit}}</p>
