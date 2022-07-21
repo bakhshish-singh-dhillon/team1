b963f18f809ca1bd49265743109ada0735cae84d
Pulkit Bhardwaj <pulkitbhardwaj3380@gmail.com>
2022-07-21 10:37:53 -0500

    404 blade file ceated

---
+++ farm-fresh/resources/views/404.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center text-center">
+        <div class="col-md-8">
+            <div class="card">
+
+                <div class="card-body">
+                    {{-- About Page Content Complete  --}}
+                    <h1>404</h1>
+                    </div>
+        </div>
+    </div>
+</div>
+@endsection
