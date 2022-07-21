ccbcd82b4848b281a9406db7f1fd874a5d5094da
Dhara Patel <dhara@Dharas-MacBook-Air.local>
2022-07-21 18:56:42 -0500

    getting reviews dynamically on detail page done

--- farm-fresh/app/Http/Controllers/ProductController.php
+++ farm-fresh/app/Http/Controllers/ProductController.php
@@ -31,7 +31,8 @@ class ProductController extends Controller
     public function show(Product $product, $id)
     {
         $products = Product::latest()->take(4)->get();
-        $reviews = Review::where('product_id', '$id')->get();
+        $reviews = Review::where('product_id', "{$id}")->latest()->take(4)->get();
+        // var_dump($reviews);
         $prod = Product::find($id);
         return view('products/show',  compact('product', 'products', 'prod', 'reviews'));
     }
