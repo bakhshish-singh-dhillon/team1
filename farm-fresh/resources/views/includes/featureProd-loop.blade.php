f56194a38ff40727fe77b8579ec1a9850eaea3c7
Dhara Patel <dhara@Dharas-MacBook-Air.local>
2022-07-21 11:11:31 -0500

    detail view added to each featured prod

--- farm-fresh/resources/views/includes/featureProd-loop.blade.php
+++ farm-fresh/resources/views/includes/featureProd-loop.blade.php
@@ -4,6 +4,7 @@
     <div class="row py-4">
         @foreach($products as $prod)
         <div class="col-md-3 px-4">
+
             <div class="card product-item shadow">
 
                 <img class="card-img-top" src="{{$images_path.$prod->images()->first()->url}}" alt="{{$prod->name}}">
@@ -11,7 +12,8 @@
                     <h5 class="card-title green-text text-bold">{{$prod->name}}</h5>
                     <p class="card-text">$ {{$prod->price}} / {{$prod->measure_unit}}</p>
                 </div>
-                <a href="" class="btn hanging-btn">View</a>
+                <a href="/products/show/{{$prod->id}}" class="btn hanging-btn product-item">View</a>
+
             </div>
         </div>
 
