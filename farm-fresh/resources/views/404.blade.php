7f19561f4791ebd65b70c739e5649f24d30fcdb3
Pulkit Bhardwaj <pulkitbhardwaj3380@gmail.com>
2022-07-21 11:52:47 -0500

    content work done for 404 page

--- farm-fresh/resources/views/404.blade.php
+++ farm-fresh/resources/views/404.blade.php
@@ -1,16 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center text-center">
-        <div class="col-md-8">
-            <div class="card">
+    <div class="container my-4">
+        <div class="row justify-content-center text-center">
+            <div class="col-md-8">
+                <div class="card">
 
-                <div class="card-body">
-                    {{-- About Page Content Complete  --}}
-                    <h1>404</h1>
+                    <div class="card-body">
+                        {{-- 404 Page Content Complete --}}
+                        <img src="images/404.png" alt="404">
+                        <h1 class="404head">This page does not exist.</h1>
+                        <p>Looks like you entered a page which doesn't exist anymore.</p>
+                        <p>Let's take you back to home.</p>
+                        <a href="/home" class="btn">Home</a>
                     </div>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
-@endsection
+    @endsection
