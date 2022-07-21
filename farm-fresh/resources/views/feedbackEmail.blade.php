c7df0931735d33c8e9a1463e1fb4522d3a4d5356
pravindra <singh280597@gmail.com>
2022-07-20 21:41:50 -0500

    added template body to emial template

--- farm-fresh/resources/views/feedbackEmail.blade.php
+++ farm-fresh/resources/views/feedbackEmail.blade.php
@@ -8,5 +8,22 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-6 col-lg-6 col-sm-12 m-auto">
+                <h3> Send Email in Laravel 8 Using Gmail </h3>
+                <p> Hey, </p>
+                <p> Welcome to Programming Fields </p>
+                <p> This is a basic demo for sending email in Laravel 8 using Gmail SMTP </p>
+                <br />
+                <br />
+                <p> Best Regards</p>
+                <p> Team, Programming Fields </p>
+            </div>
+        </div>
+    </div>
+</body>
 
 </html>
