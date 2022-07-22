c5b63db9684ebb91ca666ef380392e9008b28585
Sargam <sargam.sanghani@gmail.com>
2022-07-22 13:01:01 -0500

    Added icons to the menu list admin

--- farm-fresh/resources/views/includes/admin/header.blade.php
+++ farm-fresh/resources/views/includes/admin/header.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://kit.fontawesome.com/b23be4ec12.js" crossorigin="anonymous"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
