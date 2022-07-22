67048e882af8a91693ab47bf1eeb993b8de3e7b3
Sargam <sargam.sanghani@gmail.com>
2022-07-22 12:51:27 -0500

    Created sidebar for admin dashboard

---
+++ farm-fresh/resources/views/includes/admin/sidebar.blade.php
@@ -0,0 +1,27 @@
+<div class="row container p-0">
+    <div class="col-md-3" id="admin_sidebar">
+        <ul class="side-bar">
+            <li>
+                <a href="/admin" class="home-logo m-4">Admin Dashboard</a>
+            </li>
+            <li class="menu-item">
+                <a href="/admin">Home</a>
+            </li>
+            <li class="menu-item">
+                <a href="/admin/orders">Orders</a>
+            </li>
+            <li class="menu-item">
+                <a href="/admin/users">Users</a>
+            </li>
+            <li class="menu-item">
+                <a href="/admin/products">Products</a>
+            </li>
+            <li class="menu-item">
+                <a href="/admin/categories">Categories</a>
+            </li>
+            <li class="menu-item">
+                <a href="/admin/transaction-logs">Transaction Logs</a>
+            </li>
+        </ul>
+    </div>
+</div>
