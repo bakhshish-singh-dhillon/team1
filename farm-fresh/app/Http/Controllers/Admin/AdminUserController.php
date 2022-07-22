86b7c260f8bb9495782c2e2ef0e906cd449a45ac
Pulkit Bhardwaj <pulkitbhardwaj3380@gmail.com>
2022-07-22 10:59:30 -0500

    index function work.

--- farm-fresh/app/Http/Controllers/Admin/AdminUserController.php
+++ farm-fresh/app/Http/Controllers/Admin/AdminUserController.php
@@ -2,10 +2,30 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class AdminUserController extends Controller
 {
-    //
+    /**
+     * Instantiate a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware(['auth', 'admin']);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::latest()->paginate(10);
+        return view('admin/users/index', compact('users'));
+    }
 }
