1e2ae386a746916f587145b9b053125a73d5bccb
pravindra <singh280597@gmail.com>
2022-07-24 13:30:02 -0500

    Created index method for category listing

--- farm-fresh/app/Http/Controllers/Admin/CategoryController.php
+++ farm-fresh/app/Http/Controllers/Admin/CategoryController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class CategoryController extends Controller
 {
@@ -15,5 +16,16 @@ class CategoryController extends Controller
     public function __construct()
     {
         $this->middleware(['auth', 'admin']);
+    }
+
+    /**
+     * Display a listing of the categories.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Category::latest()->paginate(10);
+        return view('admin/category/index', compact('categories'));
     }
 }
