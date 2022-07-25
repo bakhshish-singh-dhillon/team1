64e38ebbeecf8452174441c262d75e36fd920645
pravindra <singh280597@gmail.com>
2022-07-25 12:45:57 -0500

    Added js to show modal title dynamically

--- farm-fresh/resources/js/main.js
+++ farm-fresh/resources/js/main.js
@@ -42,8 +42,20 @@ $(document).ready(function($) {
         }
     });
     // Pravindra Category Modal
-    $("#createModal").click(function() {
-        $("#exampleModalCenter").addClass('show');
-    });
-    // Pravindra Category Modal
+    const exampleModal = document.getElementById('exampleModal')
+    exampleModal.addEventListener('show.bs.modal', event => {
+        // Button that triggered the modal
+        const button = event.relatedTarget
+            // Extract info from data-bs-* attributes
+        const recipient = button.getAttribute('data-bs-whatever')
+            // If necessary, you could initiate an AJAX request here
+            // and then do the updating in a callback.
+            //
+            // Update the modal's content.
+        const modalTitle = exampleModal.querySelector('.modal-title')
+        const modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+        modalTitle.textContent = `${recipient} Category`
+        modalBodyInput.value = recipient
+    })
 });
--- farm-fresh/resources/views/admin/Categories/index.blade.php
+++ farm-fresh/resources/views/admin/Categories/index.blade.php
@@ -11,9 +11,9 @@
                 <div class="card-body">
                     <div class="d-flex justify-content-between mb-2">
                         <!-- Button trigger modal -->
-                        <button type="button" class="btn btn-primary" data-toggle="modal" id="createModal">
-                            Launch demo modal
-                        </button>
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="Create">Create Category</button>
+
+
                         <div>
                             <form method="get" action="/admin/products/">
                                 <div class="btn-group">
@@ -56,24 +56,30 @@
                         </tbody>
                     </table>
                     <!-- Button trigger modal -->
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                        Launch demo modal
-                    </button>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Open modal for @mdo</button>
 
-                    <!-- Modal -->
                     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                         <div class="modal-dialog modal-dialog-centered">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                    <h5 class="modal-title" id="exampleModalLabel">New message</h5>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
                                 <div class="modal-body">
-                                    ...
+                                    <form>
+                                        <div class="mb-3">
+                                            <label for="recipient-name" class="col-form-label">Recipient:</label>
+                                            <input type="text" class="form-control" id="recipient-name">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="message-text" class="col-form-label">Message:</label>
+                                            <textarea class="form-control" id="message-text"></textarea>
+                                        </div>
+                                    </form>
                                 </div>
                                 <div class="modal-footer">
                                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                    <button type="button" class="btn btn-primary">Send message</button>
                                 </div>
                             </div>
                         </div>
