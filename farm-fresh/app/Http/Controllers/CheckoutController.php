2176a028215b736e59d3ffcfc8e830b0442ca32f
Bakhshish Singh Dhillon <bakhshish.dev@gmail.com>
2022-07-25 11:29:53 -0500

    Checkout view file created

--- farm-fresh/app/Http/Controllers/CheckoutController.php
+++ farm-fresh/app/Http/Controllers/CheckoutController.php
@@ -17,4 +17,15 @@ class CheckoutController extends Controller
         $this->middleware(['auth']);
     }
 
+    public function index()
+    {
+        if (session()->has('cart') && count(session()->get('cart')) !== 0) {
+            $bill['subtotal'] = array_sum(array_column(session()->get('cart'), 'line_price'));
+            $bill['gst'] = 0.05 * $bill['subtotal'];
+            $bill['pst'] = 0.07 * $bill['subtotal'];
+            $bill['total'] = $bill['subtotal'] + $bill['pst'] + $bill['gst'];
+            return view('checkout', compact('bill'));
+        }
+        return back()->withError('Cart is empty');
+    }
 }
