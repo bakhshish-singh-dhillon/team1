5fd9d6fc0acb4b65b9acac436c34cd3cb754b7d5
Pulkit Bhardwaj <pulkitbhardwaj3380@gmail.com>
2022-07-25 11:13:29 -0500

    Index function created in controller

--- farm-fresh/app/Http/Controllers/Admin/AdminReviewController.php
+++ farm-fresh/app/Http/Controllers/Admin/AdminReviewController.php
@@ -2,10 +2,30 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Review;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class AdminReviewController extends Controller
 {
-    //
+    /**
+     * Instantiate a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware(['auth', 'admin']);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $reviews = Review::latest()->paginate(10);
+        return view('admin/reviews/index', compact('reviews'));
+    }
 }
