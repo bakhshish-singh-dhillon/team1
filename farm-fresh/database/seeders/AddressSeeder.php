384230e0d76004247039f97e54aebbe755e1574e
Bakhshish Singh Dhillon <bakhshish.dev@gmail.com>
2022-07-26 17:29:53 -0500

    Addresses added to seeder

--- farm-fresh/database/seeders/AddressSeeder.php
+++ farm-fresh/database/seeders/AddressSeeder.php
@@ -16,11 +16,39 @@ class AddressSeeder extends Seeder
     public function run()
     {
         DB::table('addresses')->insert([
-            'id' => '1',
-            'name' => 'Dairy',
             'user_id' => 1,
             'address_type' => "Home",
             'address' => "105 harledan drive",
+            'city' => "Winnipeg",
+            'province' => "MB",
+            'country' => "CA",
+            'postal_code' => "R4A0C9",
+            'phone' => "10983019830"
+        ]);
+        DB::table('addresses')->insert([
+            'user_id' => 1,
+            'address_type' => "Office",
+            'address' => "1 blossom drive",
+            'city' => "Winnipeg",
+            'province' => "MB",
+            'country' => "CA",
+            'postal_code' => "R4A0C9",
+            'phone' => "10983019830"
+        ]);
+        DB::table('addresses')->insert([
+            'user_id' => 1,
+            'address_type' => "School",
+            'address' => "22 Kim drive",
+            'city' => "Winnipeg",
+            'province' => "MB",
+            'country' => "CA",
+            'postal_code' => "R4A0C9",
+            'phone' => "10983019830"
+        ]);
+        DB::table('addresses')->insert([
+            'user_id' => 1,
+            'address_type' => "Parking",
+            'address' => "345 Karls drive",
             'city' => "Winnipeg",
             'province' => "MB",
             'country' => "CA",
