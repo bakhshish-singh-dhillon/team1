5387564c3b9b8e3d45bdbe6ada83c751cbc83fe4
Sargam <sargam.sanghani@gmail.com>
2022-07-28 11:31:23 -0500

    Flash message close styling done

--- farm-fresh/resources/js/main.js
+++ farm-fresh/resources/js/main.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 
-$(document).ready(function($) {
-    $("body").on("click", ".add-more", function() {
+$(document).ready(function ($) {
+    $("body").on("click", ".add-more", function () {
         var html = `
         <div class="form-outline mb-4">
             <label class="form-label" for="key[]">Attribute Name: <span class="text-danger">*</span></label>
@@ -16,7 +16,7 @@ $(document).ready(function($) {
 
     });
 
-    $("body").on("click", ".remove-attribute", function() {
+    $("body").on("click", ".remove-attribute", function () {
         $(this).parents(".form-outline").remove();
     });
 
@@ -30,13 +30,13 @@ $(document).ready(function($) {
     var buttonPlus = $("#plus");
     var buttonMinus = $("#minus");
 
-    var incrementPlus = buttonPlus.click(function() {
+    var incrementPlus = buttonPlus.click(function () {
         // alert("hello");
         var $n = $(".qty");
         $n.val(Number($n.val()) + 1);
     });
 
-    var decrementMinus = buttonMinus.click(function() {
+    var decrementMinus = buttonMinus.click(function () {
         var $n = $(".qty");
         var amount = Number($n.val());
         if (amount > 1) {
@@ -45,20 +45,20 @@ $(document).ready(function($) {
     });
 
     // Pravindra Category Modal
-    $('#exampleModal').on('show.bs.modal', function(event) {
-            var button = $(event.relatedTarget) // Button that triggered the modal
-            var recipient = button.data('whatever') // Extract info from data-* attributes
-                // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-                // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-            var modal = $(this)
-            modal.find('.modal-title').text('New message to ' + recipient)
-            modal.find('.modal-body input').val(recipient)
-        })
-        // Pravindra Category Modal
+    $('#exampleModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var recipient = button.data('whatever') // Extract info from data-* attributes
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this)
+        modal.find('.modal-title').text('New message to ' + recipient)
+        modal.find('.modal-body input').val(recipient)
+    })
+    // Pravindra Category Modal
 
 
     //Dhara : Close alert on click 
-    var hide = $(".alertClose").click(function() {
+    var hide = $(".alertClose").click(function () {
         $(".alert").hide();
 
     });
--- farm-fresh/resources/views/includes/flash.blade.php
+++ farm-fresh/resources/views/includes/flash.blade.php
@@ -1,18 +1,20 @@
 @if (session()->has('success'))
 <div class="alert alert-success error-message">
-
-    <i class="fa-solid fa-circle-check mx-2"></i> {{ session('success') }}<span class="alertClose"><i class="fa-solid fa-xmark position-absolute top-0 end-0" aria-hidden="false"></i></span>
+    <i class="fa-solid fa-circle-check mx-2"></i> {{ session('success') }}
+    <i class="fa-solid fa-xmark float-right py-1" aria-hidden="false"></i>
 </div>
 @endif
+
 @if (session()->has('warning'))
 <div class="alert alert-warning error-message">
-
-    <i class="fa-solid fa-triangle-exclamation mx-2"></i>{{ session('warning') }}<span class="alertClose"><i class="fa-solid fa-xmark position-absolute top-0 end-0"></i></span>
+    <i class="fa-solid fa-triangle-exclamation mx-2"></i>{{ session('warning') }}
+    <i class="fa-solid fa-xmark float-right py-1"></i>
 </div>
 @endif
+
 @if (session()->has('error'))
 <div class="alert alert-danger error-message">
-
-    <i class="fa-solid fa-circle-exclamation mx-2"></i> {{ session('error') }} <span class="alertClose"><i class="fa-solid fa-xmark position-absolute top-0 end-0"></i></span>
+    <i class="fa-solid fa-circle-exclamation mx-2"></i> {{ session('error') }}
+    <i class="fa-solid fa-xmark float-right py-1"></i>
 </div>
 @endif
