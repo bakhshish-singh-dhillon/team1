064871dcb8a06791e0526ea844de2a6592643789
Sargam <sargam.sanghani@gmail.com>
2022-07-29 11:41:56 -0500

    Admin review list styles fixing done

--- farm-fresh/resources/views/admin/reviews/index.blade.php
+++ farm-fresh/resources/views/admin/reviews/index.blade.php
@@ -1,64 +1,64 @@
 @extends('layouts/admin/app')
 
 @section('content')
-<div class="mx-auto container ">
-    <div class="row justify-content-center">
-        <div class="col-md-10">
-            <div class="card">
-                <div class="card-header">
-                    <h1 class="h1">Reviews</h1>
+
+<div class="d-flex justify-content-between mb-2 my-3">
+    <div class="title black-text">{{$title}} ({{ count($reviews) }})</div>
+
+    <div class="d-flex justify-content-between mb-2">
+        <!-- Button trigger modal -->
+        <div>
+            <form method="get" action="/admin/reviews/">
+                <div class="btn-group mx-2">
+                    @csrf
+                    <input class="form-control search-bar" type="search" name="search" placeholder="Search by id, name or rating" value="{{ app('request')->input('search') }}" />
+                    <button class="btn btn-success"><i class="fas fa-search"></i></button>
                 </div>
-                <div class="card-body">
-                    <span class="title ">{{$title}} ({{ count($reviews) }})</span>
-                    <form method="get" action="/admin/reviews/">
-                        <div class="btn-group">
-                            @csrf
-                            <input class="form-control w-96" type="search" name="search" placeholder="Search by id, name or rating" value="{{ app('request')->input('search') }}" />
-                            <button class="btn btn-success">Search</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+            </form>
 
-            <table class="table align-middle mb-0 bg-white">
-                <thead class="bg-light ">
-                    <tr>
-                        <th>#</th>
-                        <th>Review</th>
-                        <th>Rating</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody class="">
-                    @if (count($reviews) == 0)
-                    <tr colspan="4">No results found!</tr>
-                    @endif
-                    @foreach ($reviews as $review)
-                    <tr>
-                        <td>{{ $review->id }}</td>
-                        <td>{{ $review->review }}</td>
-                        <td>{{ $review->rating }}</td>
-                        <td>
-                            <div class="btn-group">
-                                <form method="post" action="{{ route('review-delete', ['review' => $review->id]) }}">
-                                    @csrf
-                                    @method('DELETE')
-                                    <input type="hidden" name="id" value="{{ $review->id }}" />
-                                    <button class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this post?')">Delete</button>
-                                </form>
-                            </div>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-
-            <div class="pagination content-center">
-
-                {!! $reviews->links('pagination::bootstrap-5') !!}
-
-            </div>
         </div>
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#categoryModal" data-bs-whatever="Create"><i class="fa-solid fa-plus mx-1"></i>Create</button>
     </div>
+</div>
+<div class="card">
+    <table class="table align-middle mb-0 bg-white">
+        <thead class="bg-light ">
+            <tr>
+                <th>#</th>
+                <th>Review</th>
+                <th>Rating</th>
+                <th class="w100">Action</th>
+            </tr>
+        </thead>
+        <tbody class="">
+            @if (count($reviews) == 0)
+            <tr colspan="4">No results found!</tr>
+            @endif
+            @foreach ($reviews as $review)
+            <tr>
+                <td>{{ $review->id }}</td>
+                <td>{{ $review->review }}</td>
+                <td>{{ $review->rating }}</td>
+                <td>
+                    <div class="btn-group">
+                        <form method="post" action="{{ route('review-delete', ['review' => $review->id]) }}">
+                            @csrf
+                            @method('DELETE')
+                            <input type="hidden" name="id" value="{{ $review->id }}" />
+                            <button class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this post?')"><i class="fa-solid fa-trash-can"></i></button>
+                        </form>
+                    </div>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+</div>
+
+<div class="pagination justify-content-center">
+
+    {!! $reviews->links('pagination::bootstrap-5') !!}
+
 </div>
 @endsection
