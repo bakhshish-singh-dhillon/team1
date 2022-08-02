90b4f4120d9c8693fd860e80b2cf5af67538cdc7
Pulkit Bhardwaj <pulkitbhardwaj3380@gmail.com>
2022-08-02 15:36:42 -0500

    Initial Commit

--- farm-fresh/resources/views/includes/nav.blade.php
+++ farm-fresh/resources/views/includes/nav.blade.php
@@ -1,11 +1,21 @@
 <nav id="header_nav">
     <ul>
-        <li @if(isset($title)) class="<?php if ($title && $title == 'Home') echo 'active';  ?>" @endif><a href="/">Home</a></li>
-        <li @if(isset($title)) class="<?php if ($title && $title == 'Products') echo 'active';  ?>" @endif><a href="/products">Products</a></li>
-        <li @if(isset($title)) class="<?php if ($title && $title == 'About') echo 'active';  ?>" @endif><a href="/about">About</a></li>
-        <li @if(isset($title)) class="<?php if ($title && $title == 'Contact') echo 'active';  ?>" @endif><a href="/contact">Contact</a></li>
+        <li @if (isset($title)) class="<?php if ($title && $title == 'Home') {
+            echo 'active';
+        } ?>" @endif><a href="/">Home</a></li>
+        <li @if (isset($title)) class="<?php if ($title && $title == 'Products') {
+            echo 'active';
+        } ?>" @endif><a href="/products">Products</a></li>
+        <li @if (isset($title)) class="<?php if ($title && $title == 'About') {
+            echo 'active';
+        } ?>" @endif><a href="/about">About</a></li>
+        <li @if (isset($title)) class="<?php if ($title && $title == 'Contact') {
+            echo 'active';
+        } ?>" @endif><a href="/contact">Contact</a></li>
         <?php if (Auth::check() && Auth::user()->is_admin) : ?>
-            <li @if(isset($title)) class="<?php if ($title && $title == 'Admin') echo 'active';  ?>" @endif><a href="/admin">Admin</a></li>
+        <li @if (isset($title)) class="<?php if ($title && $title == 'Admin') {
+            echo 'active';
+        } ?>" @endif><a href="/admin">Admin</a></li>
         <?php endif; ?>
     </ul>
 </nav>
