57d5faf3ebdddf64740879f546611724c151e9a3
pravindra <singh280597@gmail.com>
2022-08-03 15:34:45 -0500

    Addee user detail update on user profile

--- farm-fresh/resources/views/userProfile.blade.php
+++ farm-fresh/resources/views/userProfile.blade.php
@@ -10,15 +10,63 @@
             <div class="col-md-9">
                 <div class="title ">{{ $title }} </div>
 
-                <ul>
+                <!-- <ul>
+
                     <li>Email: {{$user->email}}</li>
                     @if($user->is_subscribed == "0")
                     <li>You are not subscribed to our newsletter. Please subscribe to know deals and much more.</li>
                     @else
                     <li>Thank you for subscribing to out newsletter! We will keep you updated.</li>
                     @endif
-                </ul>
+                </ul> -->
+                <div>
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
 
+                        <div class="row mb-3">
+                            <label for="first_name" class="col-md-4 col-form-label text-md-start">{{ __('First name') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="first_name" type="text" class="form-control @error('first_name') is-invalid @enderror" name="first_name" value="{{ old('first_name') }}" required autocomplete="first_name" autofocus>
+
+                                @error('first_name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="last_name" class="col-md-4 col-form-label text-md-start">{{ __('Last name') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="last_name" type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" value="{{ old('last_name') }}" required autocomplete="last_name" autofocus>
+
+                                @error('last_name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-start">{{ __('Email Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                    </form>
+                </div>
                 <div class="col-md-12 my-4">
                     <div class="card product-item">
                         <div class="card-header">
