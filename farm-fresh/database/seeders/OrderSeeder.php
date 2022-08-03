21b604adbc6598d4e1e8596ff55bd0c8f962a531
Dhara Patel <dhara199013@gmail.com>
2022-08-03 11:25:46 -0500

    order added via seeder

--- farm-fresh/database/seeders/OrderSeeder.php
+++ farm-fresh/database/seeders/OrderSeeder.php
@@ -22,12 +22,317 @@ class OrderSeeder extends Seeder
             'pst' => '4.9',
             'vat' => '3.5',
             'order_status' => 'Shipped',
-            'total' => '94.89',
-            'subtotal' => '70.00',
-            'billing_address' => '1792 Pambina Hwy, Winnipeg - R3T6G8, Manitoba',
-            'shipping_address' => '1792 Pambina Hwy, Winnipeg - R3T6G8,  Manitoba',
+            'total' => '4.89',
+            'subtotal' => '2.00',
+            'billing_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'shipping_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
             'auth_code' => '1234',
-            'transaction_status' => 'Successful'
+            'transaction_status' => 'Successful',
+            'created_at' => '2022-03-17 10:30:34'
+        ]);
+
+        DB::table('orders')->insert([
+            'user_id' => '2',
+            'delivery_charges' => '12.99',
+            'gst' => '3.5',
+            'pst' => '4.9',
+            'vat' => '3.5',
+            'order_status' => 'Shipped',
+            'total' => '4.89',
+            'subtotal' => '2.00',
+            'billing_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'shipping_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'auth_code' => '1234',
+            'transaction_status' => 'Successful',
+            'created_at' => '2021-08-17 10:30:34'
+        ]);
+
+        DB::table('orders')->insert([
+            'user_id' => '3',
+            'delivery_charges' => '10.99',
+            'gst' => '3.5',
+            'pst' => '4.9',
+            'vat' => '3.5',
+            'order_status' => 'Shipped',
+            'total' => '9.89',
+            'subtotal' => '7.00',
+            'billing_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'shipping_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'auth_code' => '1234',
+            'transaction_status' => 'Successful',
+            'created_at' => '2022-02-17 10:30:34'
+        ]);
+
+        DB::table('orders')->insert([
+            'user_id' => '5',
+            'delivery_charges' => '10.99',
+            'gst' => '3.5',
+            'pst' => '4.9',
+            'vat' => '3.5',
+            'order_status' => 'Shipped',
+            'total' => '9.89',
+            'subtotal' => '7.00',
+            'billing_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'shipping_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'auth_code' => '1234',
+            'transaction_status' => 'Successful',
+            'created_at' => '2022-02-17 10:30:34'
+        ]);
+
+        DB::table('orders')->insert([
+            'user_id' => '5',
+            'delivery_charges' => '10.99',
+            'gst' => '3.5',
+            'pst' => '4.9',
+            'vat' => '3.5',
+            'order_status' => 'Shipped',
+            'total' => '9.89',
+            'subtotal' => '7.00',
+            'billing_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'shipping_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'auth_code' => '1234',
+            'transaction_status' => 'Successful',
+            'created_at' => '2022-02-1 10:30:34'
+        ]);
+
+        DB::table('orders')->insert([
+            'user_id' => '6',
+            'delivery_charges' => '10.99',
+            'gst' => '3.5',
+            'pst' => '4.9',
+            'vat' => '3.5',
+            'order_status' => 'Shipped',
+            'total' => '39.89',
+            'subtotal' => '57.00',
+            'billing_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'shipping_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'auth_code' => '1234',
+            'transaction_status' => 'Successful',
+            'created_at' => '2021-09-1 10:30:34'
+        ]);
+
+        DB::table('orders')->insert([
+            'user_id' => '6',
+            'delivery_charges' => '10.99',
+            'gst' => '3.5',
+            'pst' => '4.9',
+            'vat' => '3.5',
+            'order_status' => 'Shipped',
+            'total' => '9.89',
+            'subtotal' => '7.00',
+            'billing_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'shipping_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'auth_code' => '1234',
+            'transaction_status' => 'Successful',
+            'created_at' => '2022-06-1 10:30:34'
+        ]);
+
+        DB::table('orders')->insert([
+            'user_id' => '6',
+            'delivery_charges' => '10.99',
+            'gst' => '3.5',
+            'pst' => '4.9',
+            'vat' => '3.5',
+            'order_status' => 'Shipped',
+            'total' => '9.89',
+            'subtotal' => '7.00',
+            'billing_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'shipping_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'auth_code' => '1234',
+            'transaction_status' => 'Successful',
+            'created_at' => '2022-06-1 10:30:34'
+        ]);
+
+        DB::table('orders')->insert([
+            'user_id' => '6',
+            'delivery_charges' => '10.99',
+            'gst' => '3.5',
+            'pst' => '4.9',
+            'vat' => '3.5',
+            'order_status' => 'Shipped',
+            'total' => 's9.89',
+            'subtotal' => '7.00',
+            'billing_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'shipping_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'auth_code' => '1234',
+            'transaction_status' => 'Successful',
+            'created_at' => '2022-06-1 10:30:34'
+        ]);
+
+        DB::table('orders')->insert([
+            'user_id' => '8',
+            'delivery_charges' => '10.99',
+            'gst' => '3.5',
+            'pst' => '4.9',
+            'vat' => '3.5',
+            'order_status' => 'Shipped',
+            'total' => '9.89',
+            'subtotal' => '7.00',
+            'billing_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'shipping_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'auth_code' => '1234',
+            'transaction_status' => 'Successful',
+            'created_at' => '2022-04-1 10:30:34'
+        ]);
+
+        DB::table('orders')->insert([
+            'user_id' => '7',
+            'delivery_charges' => '10.99',
+            'gst' => '3.5',
+            'pst' => '4.9',
+            'vat' => '3.5',
+            'order_status' => 'Shipped',
+            'total' => '9.89',
+            'subtotal' => '7.00',
+            'billing_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'shipping_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'auth_code' => '1234',
+            'transaction_status' => 'Successful',
+            'created_at' => '2022-04-1 10:30:34'
+        ]);
+
+        DB::table('orders')->insert([
+            'user_id' => '7',
+            'delivery_charges' => '10.99',
+            'gst' => '3.5',
+            'pst' => '4.9',
+            'vat' => '3.5',
+            'order_status' => 'Shipped',
+            'total' => '9.89',
+            'subtotal' => '4.00',
+            'billing_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'shipping_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'auth_code' => '1234',
+            'transaction_status' => 'Successful',
+            'created_at' => '2022-04-1 10:30:34'
+        ]);
+
+        DB::table('orders')->insert([
+            'user_id' => '7',
+            'delivery_charges' => '10.99',
+            'gst' => '3.5',
+            'pst' => '4.9',
+            'vat' => '3.5',
+            'order_status' => 'Shipped',
+            'total' => '9.89',
+            'subtotal' => '4.00',
+            'billing_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'shipping_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'auth_code' => '1234',
+            'transaction_status' => 'Successful',
+            'created_at' => '2022-04-1 10:30:34'
+        ]);
+
+        DB::table('orders')->insert([
+            'user_id' => '7',
+            'delivery_charges' => '10.99',
+            'gst' => '3.5',
+            'pst' => '4.9',
+            'vat' => '3.5',
+            'order_status' => 'Shipped',
+            'total' => '9.89',
+            'subtotal' => '4.00',
+            'billing_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'shipping_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'auth_code' => '1234',
+            'transaction_status' => 'Successful',
+            'created_at' => '2022-04-1 10:30:34'
+        ]);
+
+        DB::table('orders')->insert([
+            'user_id' => '7',
+            'delivery_charges' => '10.99',
+            'gst' => '3.5',
+            'pst' => '4.9',
+            'vat' => '3.5',
+            'order_status' => 'Shipped',
+            'total' => '4.89',
+            'subtotal' => '2.00',
+            'billing_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'shipping_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'auth_code' => '1234',
+            'transaction_status' => 'Successful',
+            'created_at' => '2022-04-1 10:30:34'
+        ]);
+
+        DB::table('orders')->insert([
+            'user_id' => '7',
+            'delivery_charges' => '10.99',
+            'gst' => '3.5',
+            'pst' => '4.9',
+            'vat' => '3.5',
+            'order_status' => 'Shipped',
+            'total' => '4.89',
+            'subtotal' => '2.00',
+            'billing_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'shipping_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'auth_code' => '1234',
+            'transaction_status' => 'Successful',
+            'created_at' => '2022-04-1 10:30:34'
+        ]);
+
+        DB::table('orders')->insert([
+            'user_id' => '7',
+            'delivery_charges' => '10.99',
+            'gst' => '3.5',
+            'pst' => '4.9',
+            'vat' => '3.5',
+            'order_status' => 'Shipped',
+            'total' => '5.89',
+            'subtotal' => '3.00',
+            'billing_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'shipping_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'auth_code' => '1234',
+            'transaction_status' => 'Successful',
+            'created_at' => '2022-04-1 10:30:34'
+        ]);
+
+        DB::table('orders')->insert([
+            'user_id' => '7',
+            'delivery_charges' => '10.99',
+            'gst' => '3.5',
+            'pst' => '4.9',
+            'vat' => '3.5',
+            'order_status' => 'Shipped',
+            'total' => '4.89',
+            'subtotal' => '2.00',
+            'billing_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'shipping_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'auth_code' => '1234',
+            'transaction_status' => 'Successful',
+            'created_at' => '2022-04-1 10:30:34'
+        ]);
+
+        DB::table('orders')->insert([
+            'user_id' => '7',
+            'delivery_charges' => '10.99',
+            'gst' => '3.5',
+            'pst' => '4.9',
+            'vat' => '3.5',
+            'order_status' => 'Shipped',
+            'total' => '4.89',
+            'subtotal' => '2.00',
+            'billing_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'shipping_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'auth_code' => '1234',
+            'transaction_status' => 'Successful',
+            'created_at' => '2022-04-1 10:30:34'
+        ]);
+
+        DB::table('orders')->insert([
+            'user_id' => '7',
+            'delivery_charges' => '10.99',
+            'gst' => '3.5',
+            'pst' => '4.9',
+            'vat' => '3.5',
+            'order_status' => 'Shipped',
+            'total' => '4.89',
+            'subtotal' => '2.00',
+            'billing_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'shipping_address' => '{"address_type":"Home","address":"234, 1792 Pambina Hwy","city":"WINNIPEG","province":"MB","country":"Canada","postal_code":"R3T 6G8","phone":"4313369999"}',
+            'auth_code' => '1234',
+            'transaction_status' => 'Successful',
+            'created_at' => '2022-04-1 10:30:34'
         ]);
     }
 }
