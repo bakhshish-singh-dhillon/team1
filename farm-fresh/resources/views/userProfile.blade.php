4d755781425d3cebf365ea1bb11b3b3bc3cfe032
Sargam <sargam.sanghani@gmail.com>
2022-08-04 14:38:58 -0500

    User profile page styleing done

--- farm-fresh/resources/views/userProfile.blade.php
+++ farm-fresh/resources/views/userProfile.blade.php
@@ -1,21 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<div id="products">
+<div id="profile">
     <div class="max-container py-4">
         <div class="row">
             <div class="col-md-3 left-part">
-                <div></div>
-                <div class="container category-list capitalization">
+                <div class="container capitalization profile-menu">
                     <ul class="p-0">
                         <li>
-                            <a href="#address" class="my-2 btn" data-bs-toggle="collapse"><strong>Address</strong></a>
+                            <a href="#address" class="my-2" data-bs-toggle="collapse"><strong>Address</strong></a>
                         </li>
                         <li>
-                            <a href="#review" class="my-2 btn" data-bs-toggle="collapse"><strong>Reviews</strong></a>
+                            <a href="#review" class="my-2" data-bs-toggle="collapse"><strong>Reviews</strong></a>
                         </li>
                         <li>
-                            <a href="#order" class="my-2 btn" data-bs-toggle="collapse"><strong>Orders</strong></a>
+                            <a href="#order" class="my-2" data-bs-toggle="collapse"><strong>Orders</strong></a>
                         </li>
 
                     </ul>
@@ -23,7 +22,7 @@
 
             </div>
             <div class="col-md-9">
-                <div class="title ">{{ $title }} </div>
+                <div class="title mb-3">{{ $title }} </div>
 
                 <!-- <ul>
 
@@ -100,26 +99,35 @@
                 <div class="col-md-12 my-4 ">
                     <div class="card product-item">
                         <div class="card-header">
-                            <h4 class="h4 text-center">Address</h4>
+                            <div class="sub-title m-0">Address</div>
                         </div>
                         <div id="address" class="collapse">
                             @if($addresses->isEmpty())
                             <p>There are no addresses associated to this user yet!</p>
                             @else
-                            @foreach ($addresses as $address)
                             <div class="card-body">
-                                <ul>
-                                    <li>{{$address->address_type}} address ::
-                                        {{$address->address}},
-                                        {{$address->city}},
-                                        {{$address->province}}
-                                        {{$address->postal_code}},
-                                        {{$address->country}}<br />
-                                        Phone: {{$address->phone}}
-                                    </li>
-                                </ul>
+                                <table class="table">
+                                    <thead>
+                                        <th>Type</th>
+                                        <th>Address</th>
+                                        <th>Phone</th>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($addresses as $address)
+                                        <tr>
+                                            <td>{{$address->address_type}} address</td>
+                                            <td>{{$address->address}},
+                                                {{$address->city}},
+                                                {{$address->province}}
+                                                {{$address->postal_code}},
+                                                {{$address->country}}
+                                            </td>
+                                            <td>{{$address->phone}}</td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
                             </div>
-                            @endforeach
                             @endif
                         </div>
                     </div>
@@ -128,23 +136,30 @@
                 <div class="col-md-12 my-4">
                     <div class="card product-item">
                         <div class="card-header">
-                            <h4 class="h4 text-center">Reviews</h4>
+                            <div class="sub-title m-0">Reviews</div>
                         </div>
                         <div id="review" class="collapse">
-                            @if($reviews->isEmpty())
-                            <p>There are no reviews provided by this user yet!</p>
-                            @else
-                            @foreach ($reviews as $review)
                             <div class="card-body">
-                                <ul>
-                                    <li>Date: {{$review->created_at}}<br />
-                                        Review: {{$review->review}}<br />
-                                        Rating: {{$review->rating}}
-                                    </li>
-                                </ul>
+                                <table class="table">
+                                    <thead>
+                                        <th>Date</th>
+                                        <th>Review</th>
+                                        <th>Rating</th>
+                                    </thead>
+                                    <tbody>
+                                        @if (count($reviews) == 0)
+                                        <tr colspan="4">You have not reviewed anything yet!</tr>
+                                        @endif
+                                        @foreach ($reviews as $review)
+                                        <tr>
+                                            <td>{{$review->created_at}}</td>
+                                            <td>{{$review->review}}</td>
+                                            <td>{{$review->rating}}</td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
                             </div>
-                            @endforeach
-                            @endif
                         </div>
                     </div>
                 </div>
@@ -152,24 +167,35 @@
                 <div class="col-md-12 my-4">
                     <div class="card product-item">
                         <div class="card-header">
-                            <h4 class="h4 text-center">Orders</h4>
+                            <div class="sub-title m-0">Orders</div>
                         </div>
                         <div id="order" class="collapse">
                             @if($orders->isEmpty())
                             <p>There are no orders placed by this user yet!</p>
                             @else
-                            @foreach ($orders as $order)
-
                             <div class="card-body">
-                                <ul>
-                                    <li>Date: {{$order->created_at}}<br />
-                                        Total: {{$order->total}}<br />
-                                        Status: {{$order->order_status}}<br />
-                                        Click <a href="/userOrder/{{$order->id}}">Here</a> to view more details about this order!
-                                    </li>
-                                </ul>
+                                <table class="table">
+                                    <thead>
+                                        <th>Date</th>
+                                        <th>Total</th>
+                                        <th>Status</th>
+                                        <th>View</th>
+                                    </thead>
+                                    <tbody>
+                                        @if (count($orders) == 0)
+                                        <tr colspan="4">You have not purchased anything yet!</tr>
+                                        @endif
+                                        @foreach ($orders as $order)
+                                        <tr>
+                                            <td>{{$order->created_at}}</td>
+                                            <td>$ {{$order->total}}</td>
+                                            <td>{{$order->order_status}}</td>
+                                            <td><a href="/userOrder/{{$order->id}}" class="btn">View</a> </td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
                             </div>
-                            @endforeach
                             @endif
                         </div>
                     </div>
